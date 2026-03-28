2bfdd824e33be37953209022e9f088b3f6166a5e
Daniel Peñaloza <DanP1925@gmail.com>
2026-03-28 01:36:36 +0100

    refactor: Introduce robot pattern for UI Test

---
+++ app/src/androidTest/java/com/danp1925/currencyexchange/presentation/main/MainScreenRobot.kt
@@ -0,0 +1,27 @@
+package com.danp1925.currencyexchange.presentation.main
+
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.junit4.ComposeTestRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.performTextInput
+
+class MainScreenRobot(
+    val composeTestRule: ComposeTestRule,
+) {
+    fun inputValue(newValue: String) {
+        composeTestRule
+            .onNode(hasTestTag(MainTestTag.CURRENCY_INPUT))
+            .performTextInput(text = newValue)
+    }
+
+    fun assertExchangeRateAtIndex(
+        index: Int,
+        converted: String,
+    ) {
+        composeTestRule
+            .onNode(hasTestTag(MainTestTag.EXCHANGE_RATES))
+            .onChildren()[index]
+            .assertTextEquals(converted)
+    }
+}
--- app/src/androidTest/java/com/danp1925/currencyexchange/presentation/main/MainScreenTest.kt
+++ app/src/androidTest/java/com/danp1925/currencyexchange/presentation/main/MainScreenTest.kt
@@ -1,10 +1,6 @@
 package com.danp1925.currencyexchange.presentation.main
 
-import androidx.compose.ui.test.assertTextEquals
-import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onChildren
-import androidx.compose.ui.test.performTextInput
 import com.danp1925.currencyexchange.ui.theme.CurrencyExchangeTheme
 import org.junit.Rule
 import org.junit.Test
@@ -26,15 +22,11 @@ class MainScreenTest {
         val originalCurrency = "100"
         val expectedCurrencies = listOf("345.95", "86.61", "16029.90")
 
-        composeTestRule
-            .onNode(hasTestTag(MainTestTag.CURRENCY_INPUT))
-            .performTextInput(originalCurrency)
-
-        expectedCurrencies.forEachIndexed { index, expected ->
-            composeTestRule
-                .onNode(hasTestTag(MainTestTag.EXCHANGE_RATES))
-                .onChildren()[index]
-                .assertTextEquals(expected)
+        with(MainScreenRobot(composeTestRule)) {
+            inputValue(newValue = originalCurrency)
+            expectedCurrencies.forEachIndexed { index, expected ->
+                assertExchangeRateAtIndex(index, expected)
+            }
         }
     }
 }
