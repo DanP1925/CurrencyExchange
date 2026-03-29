ec4ca99ace297fc95c8c596bcd237fd19afc532d
Daniel Peñaloza <DanP1925@gmail.com>
2026-03-29 15:59:36 +0200

    test: Add unit tests for all cases of onValueChanged

--- app/src/test/java/com/danp1925/currencyexchange/presentation/main/MainViewModelTest.kt
+++ app/src/test/java/com/danp1925/currencyexchange/presentation/main/MainViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.danp1925.currencyexchange.presentation.main
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class MainViewModelTest {
@@ -20,5 +21,53 @@ class MainViewModelTest {
         assertEquals(expectedValue, actualDisplayValue)
         assertEquals(expectedValue, actual.value.toPlainString())
         assertEquals(expectedCurrency, actual.currency)
+    }
+
+    @Nested
+    inner class OnValueChanged {
+        @Test
+        fun `GIVEN valid value WHEN onValueChanged THEN update state`() {
+            val expectedValue = "100"
+
+            sut.onValueChanged(expectedValue)
+            val result = sut.uiState.value.baseValue
+
+            assertEquals(expectedValue, result.value.toString())
+        }
+
+        @Test
+        fun `GIVEN invalid regex WHEN onValueChanged THEN early return`() {
+            val invalidRegex = "invalid_regex"
+            val expectedValue = "100"
+
+            sut.onValueChanged(expectedValue)
+            sut.onValueChanged(invalidRegex)
+            val result = sut.uiState.value.baseValue
+
+            assertEquals(expectedValue, result.value.toString())
+        }
+
+        @Test
+        fun `GIVEN value ends with dot WHEN onValueChanged THEN update displayValue`() {
+            val input = "100."
+            val expectedValue = "100"
+
+            sut.onValueChanged(expectedValue)
+            sut.onValueChanged(input)
+            val result = sut.uiState.value.baseValue
+
+            assertEquals(expectedValue, result.value.toString())
+        }
+
+        @Test
+        fun `GIVEN value is empty WHEN onValueChanged THEN update state `() {
+            val input = ""
+            val expectedValue = "0"
+
+            sut.onValueChanged(input)
+            val result = sut.uiState.value.baseValue
+
+            assertEquals(expectedValue, result.value.toString())
+        }
     }
 }
